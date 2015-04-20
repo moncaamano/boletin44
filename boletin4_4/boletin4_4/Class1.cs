bca4049d2c9fd7403fd9ae7f5a386b5df5f37dd3
moncaamano <moncaamano@yahoo.es>
2015-04-20 15:27:40 +0200

    Inicializacion Ramon Caamano

---
+++ boletin4_4/boletin4_4/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_4_4_X
+{
+    class PersonaX
+    {
+        public PersonaX(int altura, int peso, string colorOjos, string colorPelo)
+        {
+            this.alturaX = altura;
+            this.colorOjosX = colorOjos;
+            this.colorPeloX = colorPelo;
+            this.pesoX = peso;
+        }
+
+        public int pesoX;
+        public int alturaX;
+        public string colorOjosX;
+        public string colorPeloX;
+
+        public void PonerLentillasX(string colorLentillas)
+        {
+
+        }
+
+        public void EngordarX(int cantidad)
+        {
+
+        }
+
+        public void AdelgazarX(double cantidad)
+        {
+
+        }
+    }
+}
